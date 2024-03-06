20b1354a88adecd22c6762d55c6b9c9f6ea03700
Santato <santatolucas7@gmail.com>
2024-03-05 21:36:57 -0300

    remove comments

--- js/8-kyu/sum-arrays/test.js
+++ js/8-kyu/sum-arrays/test.js
@@ -1,16 +1,15 @@
 const assert = require("assert");
 const sum = require("./kata.js");
 
-// Testes
 try {
-  assert.strictEqual(sum([]), 0); // Teste para array vazio
+  assert.strictEqual(sum([]), 0);
   console.log("Test 1 passed.");
 } catch (error) {
   console.error("Test 1 failed.", error.message);
 }
 
 try {
-  assert.strictEqual(sum([1, 5.2, 4, 0, -1]), 9.2); // Teste para array com números
+  assert.strictEqual(sum([1, 5.2, 4, 0, -1]), 9.2);
   console.log("Test 2 passed.");
 } catch (error) {
   console.error("Test 2 failed.", error.message);

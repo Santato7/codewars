8a0eaa067fb7421b60bd328e2c4b0331a14396fe
Santato <santatolucas7@gmail.com>
2024-03-06 20:13:51 -0300

    add kata solution and tests

--- js/8-kyu/grasshopper-grade-book/kata.js
+++ js/8-kyu/grasshopper-grade-book/kata.js
@@ -1,3 +1,10 @@
 function getGrade(s1, s2, s3) {
-  // Code here
+  const m = (s1 + s2 + s3) / 3;
+  if (m < 60) return "F";
+  if (m < 70) return "D";
+  if (m < 80) return "C";
+  if (m < 90) return "B";
+  return "A";
 }
+
+module.exports = getGrade;
--- js/8-kyu/grasshopper-grade-book/test.js
+++ js/8-kyu/grasshopper-grade-book/test.js
@@ -1,30 +1,114 @@
-const Test = require("@codewars/test-compat");
+const assert = require("assert");
+const getGrade = require("./kata.js");
 
-describe("grade book", function () {
-  it("should return an A", function () {
-    Test.assertEquals(getGrade(95, 90, 93), "A");
-    Test.assertEquals(getGrade(100, 85, 96), "A");
-    Test.assertEquals(getGrade(92, 93, 94), "A");
-  });
-  it("should return a B", function () {
-    Test.assertEquals(getGrade(70, 70, 100), "B");
-    Test.assertEquals(getGrade(82, 85, 87), "B");
-    Test.assertEquals(getGrade(84, 79, 85), "B");
-    Test.assertEquals(getGrade(89, 89, 90), "B");
-  });
-  it("should return a C", function () {
-    Test.assertEquals(getGrade(70, 70, 70), "C");
-    Test.assertEquals(getGrade(75, 70, 79), "C");
-    Test.assertEquals(getGrade(60, 82, 76), "C");
-  });
-  it("should return a D", function () {
-    Test.assertEquals(getGrade(65, 70, 59), "D");
-    Test.assertEquals(getGrade(66, 62, 68), "D");
-    Test.assertEquals(getGrade(58, 62, 70), "D");
-  });
-  it("should return an F", function () {
-    Test.assertEquals(getGrade(44, 55, 52), "F");
-    Test.assertEquals(getGrade(48, 55, 52), "F");
-    Test.assertEquals(getGrade(58, 59, 60), "F");
-  });
-});
+try {
+  assert.strictEqual(getGrade(95, 90, 93), "A");
+  console.log("Test 1 passed.");
+} catch (error) {
+  console.error("Test 1 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(100, 85, 96), "A");
+  console.log("Test 2 passed.");
+} catch (error) {
+  console.error("Test 2 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(92, 93, 94), "A");
+  console.log("Test 3 passed.");
+} catch (error) {
+  console.error("Test 3 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(70, 70, 100), "B");
+  console.log("Test 4 passed.");
+} catch (error) {
+  console.error("Test 4 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(82, 85, 87), "B");
+  console.log("Test 5 passed.");
+} catch (error) {
+  console.error("Test 5 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(84, 79, 85), "B");
+  console.log("Test 6 passed.");
+} catch (error) {
+  console.error("Test 6 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(89, 89, 90), "B");
+  console.log("Test 7 passed.");
+} catch (error) {
+  console.error("Test 7 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(70, 70, 70), "C");
+  console.log("Test 8 passed.");
+} catch (error) {
+  console.error("Test 8 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(75, 70, 79), "C");
+  console.log("Test 9 passed.");
+} catch (error) {
+  console.error("Test 9 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(60, 82, 76), "C");
+  console.log("Test 10 passed.");
+} catch (error) {
+  console.error("Test 10 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(65, 70, 59), "D");
+  console.log("Test 11 passed.");
+} catch (error) {
+  console.error("Test 11 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(66, 62, 68), "D");
+  console.log("Test 12 passed.");
+} catch (error) {
+  console.error("Test 12 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(58, 62, 70), "D");
+  console.log("Test 13 passed.");
+} catch (error) {
+  console.error("Test 13 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(44, 55, 52), "F");
+  console.log("Test 14 passed.");
+} catch (error) {
+  console.error("Test 14 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(48, 55, 52), "F");
+  console.log("Test 15 passed.");
+} catch (error) {
+  console.error("Test 15 failed.", error.message);
+}
+
+try {
+  assert.strictEqual(getGrade(58, 59, 60), "F");
+  console.log("Test 16 passed.");
+} catch (error) {
+  console.error("Test 16 failed.", error.message);
+}
